d4800f0bc41e149217e624bf2659f83088bfb50c
wallman1 <william.coote5@education.nsw.gov.au>
2025-03-18 13:14:47 +1100

    Stuff

--- Functions.py
+++ Functions.py
@@ -10,15 +10,34 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure((0, 1), weight=1)
 
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button_1 = customtkinter.CTkButton(self, text="Button 1")
+        self.button_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button_2 = customtkinter.CTkButton(self, text="Button 2")
+        self.button_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
         self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+        def button_callback(self):
+           scene2.mainloop()
 
-    def button_callback(self):
-        print("button pressed")
 
+class Scene2(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("my app")
+        self.geometry("400x180")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
+
+        self.button_1 = customtkinter.CTkButton(self, text="Button 1", command=self)
+        self.button_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button_2 = customtkinter.CTkButton(self, text="Button 2")
+        self.button_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+        def button_callback(self):
+           app.mainloop() 
+
+scene2 = Scene2()
 app = App()
 app.mainloop()
